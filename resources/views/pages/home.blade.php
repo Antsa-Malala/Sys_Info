bfb1e21cfe41425fe3952fa176377b314ac976e4
Tendrynyavo <94636671+Tendrynyavo@users.noreply.github.com>
2023-03-15 05:15:03 +0300

    Affichage de l'header

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/home.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <h1 class="title">Koppen IT</h1>
+        <div class="option">
+            <div><a href="#">Plan</a></div>
+            <div><a href="#">Code Journaux</a></div>
+            <div><a href="#">Journal</a></div>
+        </div>
+    </header>
+    <!-- <div class="search">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+        <form action="">
+        <input type="search" required>
+        <i class="fa fa-search"></i>
+        <a href="javascript:void(0)" id="clear-btn">Clear</a>
+        </form>
+    </div> -->
+    <!-- <div class="home">
+        <img src="./assets/img/stressed-person-using-computer-at-desk.svg" alt="">
+    </div> -->
+</body>
+</html>
