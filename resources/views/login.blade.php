9d8a4d1ade6d6a513633f27a71e6a9e7e72e8fbc
Tendrynyavo <94636671+Tendrynyavo@users.noreply.github.com>
2023-03-15 03:09:00 +0300

    affichage login user

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../système d'information/assets/css/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="super">
+        <div class="formulaire">
+            <h1 class="title">Welcome to IKOPPEN IT</h1>
+            <form action="login.php" method="POST">
+                <div class="input">
+                    <input type="text" name="username" placeholder="Username">
+                </div>
+                <div class="input">
+                    <input type="password" name="password" placeholder="Password">
+                </div>
+                <div class="input">
+                    <input type="submit" value="Ok" class="btn dark">
+                </div>
+                <h3 class="account">Don't have account <a href="#">Sign up</a></h3>
+            </form>
+        </div>
+        <div class="bg-gradient">
+            <img src="../système d'information/assets/img/logo-white.png" alt="">
+        </div>
+    </div>
+</body>
+</html>
