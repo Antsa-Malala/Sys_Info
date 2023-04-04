5538f768e6cb65377b75e0f50171eb791d0dc2b4
Tendrynyavo <Tendrirazafimiandrisoa@gmail.com>
2023-04-04 23:42:26 +0300

    Journal et code Journal

--- resources/views/pages/journal/index.blade.php
+++ resources/views/pages/journal/index.blade.php
@@ -4,32 +4,44 @@
 		<div class="container">
 			<div class="container">
 				<div class="title">
-					<h2 class="text-center text-decoration-underline">
+					<h2 class="text-center">
 						Consulter votre journal
 					</h2>
 				</div>
-				<div class="col-lg-6 text-center">
-					<table class="table table-hover">
-						<thead>
-							<th> Code Journal </th>
-							<th> Libelle Journal </th>
-						</thead>
-						<tbody>
+				<div class="mt-4">
+					<a href="{{ url('/plan-insertion') }}" class="btn btn-success me-3">Ajouter</a>
+					<div class="shadow pt-4 mt-4" style="border-radius: 15px">
+						<table class="table">
+							<tr>
+								<th class="text-center"> Code Journal </th>
+								<th class="text-center"> Libelle Journal </th>
+								<th> </th>
+								<th> </th>
+							</tr>
 							@for($i = 0 ; $i < count($journaux) ; $i++)
 									<tr>
-										<td>
+										<td class="text-center">
 											<a href="{{ url('journaux-By-Code/'.$journaux[$i]->code) }}" class="text-decoration-none">
 												{{ $journaux[$i]->code }}				
 											</a>
 										</td>
-										<td>
+										<td class="text-center">
 											{{ $journaux[$i]->libelle }}				
 										</td>
 									</tr>
 							@endfor
-						</tbody>
-					</table>
-				</div>	
+						</table>
+					</div>
+					<nav class="d-flex justify-content-center mt-4">
+						<ul class="pagination pagination-lg">
+						  <li class="page-item active" aria-current="page">
+							<span class="page-link">1</span>
+						  </li>
+						  <li class="page-item"><a class="page-link" href="#">2</a></li>
+						  <li class="page-item"><a class="page-link" href="#">3</a></li>
+						</ul>
+					</nav>
+				</div>
 			</div>
 		</div>
 	</div>
