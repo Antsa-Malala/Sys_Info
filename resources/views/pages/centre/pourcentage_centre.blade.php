eb5b2969fea5a8cead34695f7ed3a3b2bc40cbd0
Tendrynyavo <Tendrirazafimiandrisoa@gmail.com>
2023-05-06 20:48:26 +0300

    new file

---
+++ resources/views/pages/centre/pourcentage_centre.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<div class="container" style="margin-top: 125px;">
+        <div class="container w-50 shadow p-4" style="border-radius: 10px;">
+            <form action="" method="POST" id="form">
+                @csrf
+                <div class="my-3">
+                    <h4 class="text-center mb-4">Pourcentage de centre de "Nom de charge"</h4>
+					<div class="row ms-3" id="original">
+						<div class="col-md-5">
+							<select name="produit" id="" class="form-select">
+								{{-- Boucler les produits --}}
+								@for( $i = 0 ; $i < count($centres) ; $i++ )
+									<option value="{{ $centres[$i]->idcentre }}">{{ $centres[$i]->nomcentre }}</option>
+								@endfor
+							</select>
+						</div>
+						<div class="col-md-4">
+							<input type="number" name="pourcentage" class="form-control" max="100" min="0" required>
+						</div>
+						<div class="col-md-3">
+							<span onclick="clone()" class="btn btn-dark">
+								+
+							</span>
+						</div>
+					</div>
+                </div>
+				<div class="mb-3 ms-4">
+					<input type="submit" value="Valider" class="btn btn-success">
+                </div>
+            </form>
+			<div class="mb-3 ms-4">
+				<a href="/centre_form"><button class="btn btn-primary">Ajouter de centre produit</button></a>
+			</div>
+        </div>
+    </div>
+	<script src="{{ URL::asset('js/app.js') }}"></script>
+@endsection
