e82ea1289a65f15f7eb7c293a10f29f227fc6b47
Antsa-Malala <119752846+Antsa-Malala@users.noreply.github.com>
2023-05-06 14:49:41 +0300

    Ty no tena izy azafady

--- base.sql
+++ base.sql
@@ -20,7 +20,7 @@ id SERIAL primary key,
 idProduit INT,
 idNature int,
 idCentre int,
-idCharge int,
+idCharge varchar(5),
 montant double precision not null default 0,
 foreign key(idProduit) references produit(idProduit),
 foreign key(idNature) references Nature(idNature),
@@ -28,22 +28,22 @@ foreign key(idCentre) references Centre(idCentre),
 foreign key(idCharge) references plan(compte)
 );
 
-create table pourcentage_produit{
+create table pourcentage_produit(
     id serial primary key,
     idproduit int,
-    idcharge int,
+    idcharge varchar(5),
     pourcentage double precision not null default 0,
     foreign key(idproduit) references produit(idproduit),
     foreign key(idCharge) references plan(compte)
-}
+);
 
-create table pourcentage_centre{
+create table pourcentage_centre(
     id serial primary key,
     idproduit int,
-    idcharge int,
+    idcharge varchar(5),
     idCentre int,
     pourcentage double precision not null default 0,
     foreign key(idproduit) references produit(idproduit),
-    foreign key idCharge references plan(compte),
-    foreign key idCentre references Centre(idCentre)
-}
+    foreign key(idCharge) references plan(compte),
+    foreign key(idCentre) references Centre(idCentre)
+);
