496383eb76cb38c0d8f8101a96476de6d10d8b83
Tendrynyavo <Tendrirazafimiandrisoa@gmail.com>
2023-05-16 00:55:36 +0300

    resultat

--- resources/views/pages/operations/addoperation.blade.php
+++ resources/views/pages/operations/addoperation.blade.php
@@ -115,6 +115,44 @@
 	</div>
 	<script src="{{ URL::asset('js/app.js') }}"></script>
 	<script src="{{ URL::asset('js/Operations.js') }}"></script>
+	<!-- Scrollable modal -->
+	<!-- Button trigger modal -->
+	<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+		Launch demo modal
+  	</button>
+  
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-scrollable">
+		<div class="modal-content">
+			<div class="modal-header">
+			<h1 class="modal-title fs-5" id="exampleModalLabel">Charge</h1>
+			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body p-5">
+				<div class="card">
+					<div class="card-body shadow d-flex justify-content-between">
+						<div class="mt-1">
+							Nom Charge
+						</div>
+						<button class="btn btn-primary">Modifier</button>
+					</div>
+				</div>
+				<div class="card mt-4">
+					<div class="card-body shadow d-flex justify-content-between">
+						<div class="mt-1">
+							Nom Charge
+						</div>
+						<button class="btn btn-primary">Modifier</button>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+			</div>
+		</div>
+		</div>
+	</div>
 	
 	{{-- <script src="{{ URL::asset('js/operation.template.js') }}"></script> --}}
 @endsection
